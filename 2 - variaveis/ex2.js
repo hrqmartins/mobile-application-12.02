05021e292af9450cf73476d9b13f1f6d9d430ad8
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:14:28 -0300

    Exercicio 2 - programa que simula um placar de jogo.

---
+++ 2 - variaveis/ex2.js
@@ -0,0 +1,16 @@
+/*
+Crie um programa que simule um placar de jogo:
+Declare uma const chamada nomeJogador com o valor 'Ana'
+Declare um let chamado pontuacao com valor inicial 0
+Exiba o nome e a pontuação inicial
+Atualize a pontuação para 10 e exiba novamente
+*/
+
+const nomeJogador = "Ana"
+let pontuacao = 0
+
+console.log("Nome do Jogador: " + nomeJogador)
+console.log("Pontuação Inicial: " + pontuacao)
+
+pontuacao = 7
+console.log("Nova Pontuação: " + pontuacao)
