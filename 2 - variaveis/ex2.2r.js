c2feaac28ca39259ac83df000b350c8f7c169393
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:15:37 -0300

    Exemplo Resolvido 2.2

---
+++ 2 - variaveis/ex2.2r.js
@@ -0,0 +1,10 @@
+/**
+ * O operador typeof revela o tipo de uma variável em tempo de execução. É útil para depurar e entender como o JavaScript categoriza os dados.
+ */
+
+let cidade = "São Paulo";
+let populacao = 12300000;
+const ativa = true;
+console.log(typeof cidade);     // "string"
+console.log(typeof populacao);  // "number"
+console.log(typeof ativa);      // "boolean"
