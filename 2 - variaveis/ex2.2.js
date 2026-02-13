908994d1c00548ba3afc903c4bf4f9d2b41cb123
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:20:09 -0300

    Exercicio 2.2 - Exibe o tipo da variavel.

---
+++ 2 - variaveis/ex2.2.js
@@ -0,0 +1,17 @@
+/*
+Crie 4 variáveis com tipos diferentes e use typeof para descobrir o tipo de cada uma:
+Uma variável com seu nome (String)
+Uma variável com sua idade (Number)
+Uma variável com true ou false (Boolean)
+Uma variável sem valor atribuído (undefined)
+*/
+
+let nome = "Henrique"
+let idade = 18
+let maiorIdade = true
+let undefined
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof maiorIdade)
+console.log(typeof undefined)
