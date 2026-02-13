dd36756365501c7add73efc5d2a4a6444b7c22bc
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:07:40 -0300

    Exercicio 1.3 - Programa que exibe resultado de operações matemáticas.

---
+++ ex.1.3.js
@@ -0,0 +1,10 @@
+/*
+Use console.log() para exibir os resultados das seguintes operações:
+A divisão de 100 por 4
+O resultado de 7 elevado ao quadrado (7 * 7)
+O resto da divisão de 17 por 5 (use o operador %)
+*/
+
+console.log(100/4)
+console.log(7*7)
+console.log(17%5)
