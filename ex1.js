80a8c699c440ec1c9457cc6ff3f3cfaf8061bf45
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:02:29 -0300

    Exercicio 1 - Programa que exibe meu nome, curso e uma frase com console.log

---
+++ ex1.js
@@ -0,0 +1,9 @@
+/*
+Escreva um programa com 3 comandos console.log que exibam:
+Seu nome
+Seu curso
+A frase: "Estou aprendendo JavaScript!"
+*/
+console.log("Henrique Martins")
+console.log("Análise e Desenvolvimento de Sistemas")
+console.log("Estou aprendendo JavaScript!")
