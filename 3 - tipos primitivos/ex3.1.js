bfd4c7bef27ef8f4a89314bdfab594c26ba20b9b
hrqmartins <hrqmartins129@gmail.com>
2026-02-12 22:24:52 -0300

    Exercicio 3.1 - Simulação de um sistema de delivery

---
+++ 3 - tipos primitivos/ex3.1.js
@@ -0,0 +1,18 @@
+/*
+Simule um sistema de delivery:
+Declare enderecoEntrega como null (ainda sem endereço)
+Declare numeroPedido sem valor (undefined)
+Exiba ambas as variáveis antes de preencher
+Preencha o endereço e o número do pedido com valores reais
+Exiba novamente após preencher
+*/
+
+let enderecoEntrega = null
+let numeroPedido 
+console.log(enderecoEntrega)
+console.log(numeroPedido)
+
+enderecoEntrega = "Avenida Paulista - 2001"
+numeroPedido = 393
+console.log(enderecoEntrega)
+console.log(numeroPedido)
