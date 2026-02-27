73e3dc1092d6dbfe25a10d60959e1514718fe0f1
hrqmartins <hrqmartins129@gmail.com>
2026-02-26 21:32:29 -0300

    Exercicio 6.1 - Programa que resolve situação A e B

---
+++ 6 - laços de repetição/ex6.1.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+let alunos = ["Ana", "Bruno", "Clara", "Diego"];
+
+for (let i = 0; i < alunos.length; i++) {
+  console.log(`${i}: ${alunos[i]}`);
+}
+
+// Saída:
+// 0: Ana
+// 1: Bruno
+// 2: Clara
+// 3: Diego
