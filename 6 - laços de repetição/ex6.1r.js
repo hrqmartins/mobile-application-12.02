6eb4aad9060446d8767ae592cb75b73814094b98
hrqmartins <hrqmartins129@gmail.com>
2026-02-26 21:29:57 -0300

    Exercicio Resolvido 6.1

---
+++ 6 - laços de repetição/ex6.1r.js
@@ -0,0 +1,14 @@
+/*
+O for é usado quando sabemos exatamente quantas vezes o bloco deve ser executado. Ele é composto por três partes: 
+inicialização, condição e incremento.
+*/
+
+for (let i = 0; i < 5; i++) {
+  console.log("Contador:", i);
+}
+// Saída:
+// Contador: 0
+// Contador: 1
+// Contador: 2
+// Contador: 3
+// Contador: 4
